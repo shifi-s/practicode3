6949473fdc4e154e2818011ba36cc60f8b5ee4c9
shifi-s <s0548493359@gmail.com>
2025-02-09 03:34:04 +0200

    ooooof

--- ToDoList-client/src/service.js
+++ ToDoList-client/src/service.js
@@ -1,26 +1,33 @@
 import axios from 'axios';
 
-const apiUrl = "https://localhost:7271"
+axios.defaults.baseURL = "http://localhost:5146"
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+      console.error("API Error:", error.response?.status, error.message);
+      return Promise.reject(error);
+  }
+);
 
 export default {
   getTasks: async () => {
-    const result = await axios.get(`${apiUrl}/items`)    
+    const result = await axios.get(`/ToDos`)    
     return result.data;
   },
 
   addTask: async(name)=>{
-    console.log('addTask', name)
-    //TODO
+    console.log('addTask', )
+    await axios.post(`/ToDos`,{name:name,isComplete:false})
     return {};
   },
 
-  setCompleted: async(id, isComplete)=>{
-    console.log('setCompleted', {id, isComplete})
-    //TODO
-    return {};
+  setCompleted: async(id,isComplete)=>{
+    const result = await axios.put(`/ToDos/${id}?iscomplete=${isComplete}`, {});
+    return result.data;
   },
 
-  deleteTask:async()=>{
-    console.log('deleteTask')
+  deleteTask:async(id)=>{
+    await axios.delete(`/ToDos/${id}`,id)
   }
 };
--- ToDoList-server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ ToDoList-server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("ca9e1151-1572-4617-865f-df76c27c0876")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f0979dd5fdf915fefeb49c960f25f266bd18466")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ccb862f2b3f8f29020d7ec21208057ad9ed186e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
